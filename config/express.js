96b757f105f56841f21e21821f13b4792073d193
Davi Kawasaki <davishinjik@gmail.com>
2017-09-11 12:25:05 -0300

    Starting basic configs with public index render

---
+++ config/express.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var load = require('express-load');
+var bodyParser = require('body-parser');
+
+module.exports = function() {
+    var app = express();
+
+    app.use(express.static('./app/public'));
+    app.set('view engine', 'ejs');
+    app.set('views', './app/views');
+
+    app.use(bodyParser.urlencoded({extended: true}));
+    app.use(bodyParser.json());
+
+    load('routes', {cwd: 'app'})
+        .into(app);
+    
+    return app;
+}
